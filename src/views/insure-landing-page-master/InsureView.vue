502714314a2423425477afafebca2030dd0f8c63
Damndaniel241 <lombardia241@gmail.com>
2024-11-20 21:23:14 -0800

    completed desktop responsiveness for insure

--- src/views/insure-landing-page-master/InsureView.vue
+++ src/views/insure-landing-page-master/InsureView.vue
@@ -5,13 +5,18 @@ import IconHamburger from "./images/icon-hamburger.svg";
 import IconClose from "./images/icon-close.svg";
 import BgPatternMobileNav from "./images/bg-pattern-mobile-nav.svg";
 import HeroImageMobile from "./images/image-intro-mobile.jpg";
+import HeroImageDesktop from "./images/image-intro-desktop.jpg";
 import BgPatternIntroRightMobile from "./images/bg-pattern-intro-right-mobile.svg";
+import BgPatternIntroRightDesktop from "./images/bg-pattern-intro-right-desktop.svg";
 import BgPatternIntroLeftMobile from "./images/bg-pattern-intro-left-mobile.svg";
+import BgPatternIntroLeftDesktop from "./images/bg-pattern-intro-left-Desktop.svg";
 import IconSnappyProcess from "./images/icon-snappy-process.svg";
 import IconPeopleFirst from "./images/icon-people-first.svg";
 import IconAffordablePrices from "./images/icon-affordable-prices.svg";
 import BgPatternWorkMobile from "./images/bg-pattern-how-we-work-mobile.svg";
+import BgPatternWorkDesktop from "./images/bg-pattern-how-we-work-desktop.svg";
 import BgPatternFooterMobile from "./images/bg-pattern-footer-mobile.svg";
+import BgPatternFooterDesktop from "./images/bg-pattern-footer-desktop.svg";
 import IconFacebook from "./images/icon-facebook.svg";
 import IconInstagram from "./images/icon-instagram.svg";
 import IconPinterest from "./images/icon-pinterest.svg";
@@ -23,14 +28,39 @@ function clickMenu() {
 }
 </script>
 <template>
-  <nav class="flex justify-between bg-[#fafafa] p-4 items-center">
+  <nav
+    class="flex justify-between  p-4 items-center lg:w-[70em] lg:mx-auto"
+  >
     <img :src="logo" alt="insure logo" class="" />
     <img
       @click="clickMenu"
       :src="hamburgerBtnClicked ? IconClose : IconHamburger"
       alt="icon hamburger"
-      class="cursor-pointer"
+      class="cursor-pointer lg:hidden"
     />
+
+    <div class="lg:flex items-center hidden">
+      <div
+        class="text-center hover:text-[#2d2640] cursor-pointer py-2 font-mono px-5 uppercase hover:font-bold w-fit text-base tracking-widest"
+      >
+        How we work
+      </div>
+      <div
+        class="text-center hover:text-[#2d2640] cursor-pointer py-2 font-mono px-5 uppercase hover:font-bold w-fit text-base tracking-widest"
+      >
+        Blog
+      </div>
+      <div
+        class="text-center hover:text-[#2d2640] cursor-pointer py-2 font-mono px-5 uppercase hover:font-bold w-fit text-base tracking-widest"
+      >
+        Account
+      </div>
+      <div
+        class="py-1 px-5 text-center border-2 border-[#2d2640] hover:bg-[#2d2640] hover:text-[#fafafa] cursor-pointer font-mono text-base tracking-wide uppercase"
+      >
+        View plans
+      </div>
+    </div>
   </nav>
 
   <div v-if="hamburgerBtnClicked" id="mobile-menu" class="bg-[#2b272f]">
@@ -50,13 +80,17 @@ function clickMenu() {
     </div>
   </div>
 
-  <section id="hero-image">
-    <img :src="HeroImageMobile" alt="hero image mobile" class="w-full" />
+  <section id="hero-image  ">
+    <img
+      :src="HeroImageMobile"
+      alt="hero image mobile"
+      class="w-full lg:hidden"
+    />
   </section>
 
   <section
     id="humanizing-your-insurance"
-    class="bg-[#2d2640] relative flex justify-center items-center h-[70%] overflow-y-visible"
+    class="bg-[#2d2640] relative flex justify-center items-center h-[70%] overflow-y-visible lg:hidden"
   >
     <img
       :src="BgPatternIntroLeftMobile"
@@ -68,59 +102,102 @@ function clickMenu() {
       alt="bg-pattern-intro-right-mobile"
       class="absolute top-[300px] right-px"
     />
-    <div class="text-[#fafafa] flex-col gap-4 flex mx-5 text-center">
-      <h1 class="dm-serif-display-regular text-5xl">
+
+    <div class="text-[#fafafa] flex-col gap-4 flex mx-5 text-center ">
+      <h1 class="dm-serif-display-regular text-5xl lg:text-6xl">
         Humanizing your insurance.
       </h1>
-      <p class="leading-8 text-[#fafafa] ">
+      <p class="leading-8 text-[#fafafa]">
         Get your life insurance coverage easier and faster. We blend our
         expertise and technology to help you find the plan that’s right for you.
         Ensure you and your loved ones are protected.
       </p>
       <div
-        class=" hover:bg-[#fafafa] hover:text-[#2d2640] cursor-pointer border-2 py-2 font-mono px-5 uppercase text-center font-bold w-fit text-base mx-auto tracking-widest"
+        class="hover:bg-[#fafafa] hover:text-[#2d2640] cursor-pointer border-2 py-2 font-mono px-5 uppercase text-center font-bold w-fit text-base mx-auto tracking-widest"
       >
         View plans
       </div>
     </div>
   </section>
 
-  <Section class="h-[20%]" />
+  <section id="humanizing-your-insurance-desktop" class="hidden lg:block bg-[#2d2640] relative ">
+    <img
+      :src="BgPatternIntroLeftDesktop"
+      alt="bg-pattern-intro-left-desktop"
+      class="absolute top-[450px] left-px"
+    />
+    <img
+      :src="BgPatternIntroRightDesktop"
+      alt="bg-pattern-intro-right-Desktop"
+      class="absolute top-px right-px"
+    />
 
-  <section id="were-different" class="">
-    <div class="flex-col flex gap-5 items-center">
-      <div class="border-t w-[40%] border-t-[#837d87]" />
-      <h1 class="text-5xl dm-serif-display-regular my-4 text-[#2b272f]">
+    <div class="w-[70em] flex mx-auto max-h-[39.722rem] ">
+      <div class="w-[35em] mx-4 flex  items-center">
+        <div class="text-[#fafafa] flex-col gap-5 flex   ">
+          <div class="border-t w-[30%]  border-t-[#837d87]" />
+          <h1 class="dm-serif-display-regular lg:text-7xl">
+            Humanizing your insurance.
+          </h1>
+          <p class="leading-8 text-[#fafafa]">
+            Get your life insurance coverage easier and faster. We blend our
+            expertise and technology to help you find the plan that’s right for
+            you. Ensure you and your loved ones are protected.
+          </p>
+          <div
+            class="hover:bg-[#fafafa] hover:text-[#2d2640] cursor-pointer border-2 py-2 font-mono px-5 uppercase text-center font-bold w-fit text-base  tracking-widest"
+          >
+            View plans
+          </div>
+        </div>
+      </div>
+      <section id="hero-image" class="w-[35em] mx-4 ">
+    <img
+      :src="HeroImageDesktop"
+      alt="hero image desktop "
+      class="w-full  mt-[9.08rem]"
+    />
+  </section>
+
+    </div>
+  </section>
+
+  <Section class="h-[20%] lg:h-[50%]" />
+
+  <section id="were-different" class="lg:w-[70em] mx-auto">
+    <div class="flex-col flex gap-5 lg:gap-[2em] ms-4 items-center lg:items-start">
+      <div class="border-t w-[40%] lg:w-[15%]  border-t-[#837d87]" />
+      <h1 class="text-5xl dm-serif-display-regular my-4 text-[#2b272f]  lg:text-7xl">
         We’re different
       </h1>
     </div>
-    <div class="flex flex-col">
-      <div class="flex flex-col space-y-9 items-center pt-[4em]">
+    <div class="flex flex-col lg:flex-row lg:w-[70em] mx-auto">
+      <div class="flex flex-col space-y-9 items-center lg:items-start  pt-[4em] lg:ms-4">
         <img :src="IconSnappyProcess" alt="" class="w-30" />
         <p class="text-3xl dm-serif-display-regular text-[#2b272f]">
           Snappy Process
         </p>
-        <p class="text-[#837d87] text-center text-lg w-[90%]">
+        <p class="text-[#837d87] text-center lg:text-left text-lg w-[90%] lg:w-full">
           Our application process can be completed in minutes, not hours. Don’t
           get stuck filling in tedious forms.
         </p>
       </div>
-      <div class="flex flex-col space-y-9 items-center pt-[4em]">
+      <div class="flex flex-col space-y-9 items-center lg:items-start pt-[4em] lg:ms-4">
         <img :src="IconAffordablePrices" alt="" class="w-30" />
         <p class="text-3xl dm-serif-display-regular text-[#2b272f]">
           Affordable Prices
         </p>
-        <p class="text-[#837d87] text-center text-lg w-[90%]">
+        <p class="text-[#837d87] text-center lg:text-left text-lg w-[90%] lg:w-full">
           We don’t want you worrying about high monthly costs. Our prices may be
           low, but we still offer the best coverage possible.
         </p>
       </div>
-      <div class="flex flex-col space-y-9 items-center pt-[4em]">
+      <div class="flex flex-col space-y-9 items-center lg:items-start pt-[4em] lg:ms-4">
         <img :src="IconPeopleFirst" alt="" class="w-30" />
         <p class="text-3xl dm-serif-display-regular text-[#2b272f]">
           People First
         </p>
-        <p class="text-[#837d87] text-center text-lg w-[90%]">
+        <p class="text-[#837d87] text-center lg:text-left text-lg w-[90%] lg:w-full">
           Our plans aren’t full of conditions and clauses to prevent payouts. We
           make sure you’re covered when you need it.
         </p>
@@ -130,16 +207,17 @@ function clickMenu() {
 
   <section id="how-we-work" class="flex justify-center items-center h-[90%]">
     <div
-      class="bg-[#2d2640] relative text-[#fafafa] w-[90%] flex-col flex px-[2em] py-[5em] space-y-8 items-center"
+      class="bg-[#2d2640] relative text-[#fafafa] w-[90%] flex-col flex px-[2em] py-[5em] lg:p-[4em] lg:h-fit space-y-8 lg:space-y-0 items-center lg:flex-row lg:justify-between lg:mx-auto lg:w-[70em]"
     >
-      <img :src="BgPatternWorkMobile" alt="" class="absolute top-px right-px" />
+      <img :src="BgPatternWorkMobile" alt="" class="absolute top-px right-px lg:hidden" />
+      <img :src="BgPatternWorkDesktop" alt="" class="absolute bottom-px right-px h-full  hidden lg:block" />
 
-      <h1 class="dm-serif-display-regular text-5xl h-[70%] text-center">
+      <h1 class="dm-serif-display-regular text-5xl h-[70%] lg:h-[100%] text-center lg:text-left lg:w-[50%] ">
         Find out more about how we work
       </h1>
 
       <div
-        class=" hover:bg-[#fafafa] hover:text-[#2d2640] font-mono font-bold text-base border-2 py-2 px-5 uppercase text-center w-fit mx-auto tracking-widest"
+        class="hover:bg-[#fafafa] lg:cursor-pointer  hover:text-[#2d2640] font-mono font-bold text-base border-2 py-2 px-5 uppercase text-center w-fit mx-auto lg:mx-0 tracking-widest"
       >
         how we work
       </div>
@@ -147,28 +225,34 @@ function clickMenu() {
   </section>
 
   <footer class="bg-[#fafafa] relative">
-    <img :src="BgPatternFooterMobile" alt="" class="top-px absolute w-full" />
+    <img :src="BgPatternFooterMobile" alt="" class="top-px absolute w-full lg:hidden" />
+    <img :src="BgPatternFooterDesktop" alt="" class="top-px left-px  absolute  hidden lg:block" />
     <section
       id="social-media"
-      class="flex flex-col justify-center items-center gap-[2em] p-[5em]"
+      class="flex flex-col justify-center items-center gap-[2em] p-[5em]  lg:gap-0 lg:p-0 lg:flex-row lg:justify-between lg:w-[70em] lg:mx-auto lg:py-[3em]"
     >
       <img :src="logo" alt="insure logo" class="" />
 
       <div class="flex gap-3">
-        <img :src="IconFacebook" alt="" class="hover:text-[#2d2640] cursor-pointer" >
+        <img
+          :src="IconFacebook"
+          alt=""
+          class="hover:text-[#2d2640] cursor-pointer"
+        />
         <img :src="IconTwitter" alt="" class="" />
         <img :src="IconPinterest" alt="" class="" />
         <img :src="IconInstagram" alt="" class="" />
       </div>
-      <div class="border-t w-full border-t-[#837d87]" />
+      <div class="border-t w-full border-t-[#837d87] lg:hidden" />
     </section>
+    <div class="border-t lg:w-[70em] hidden lg:block lg:mx-auto border-t-[#837d87] mb-[3em]" />
 
-    <section id="info" class="uppercase flex flex-col items-center">
+    <section id="info" class="uppercase flex flex-col lg:w-[70em] lg:flex-row lg:mx-auto lg:justify-between items-center">
       <div
-        class="flex flex-col gap-[1.5em] items-center font-mono font-bold text-lg tracking-wider mb-[3em]"
+        class="flex flex-col gap-[1.5em] items-center lg:items-start font-mono font-bold text-lg tracking-wider mb-[3em]"
       >
         <p class="text-[#837d87]">Our company</p>
-        <div class="flex flex-col items-center text-lg text-[#2d2640]">
+        <div class="flex flex-col items-center lg:items-start text-lg text-[#2d2640]">
           <p>How we work</p>
           <p>Why Insure?</p>
           <p>check price</p>
@@ -176,10 +260,10 @@ function clickMenu() {
         </div>
       </div>
       <div
-        class="flex flex-col gap-[1.5em] items-center font-mono font-bold text-lg tracking-wider mb-[3em]"
+        class="flex flex-col gap-[1.5em] items-center lg:items-start font-mono font-bold text-lg tracking-wider mb-[3em]"
       >
-        <p class="text-[#837d87] ">help me</p>
-        <div class="flex flex-col items-center text-lg text-[#2d2640]">
+        <p class="text-[#837d87]">help me</p>
+        <div class="flex flex-col items-center lg:items-start text-lg text-[#2d2640]">
           <p>faq</p>
           <p>terms of use</p>
           <p>privacy policy</p>
@@ -187,20 +271,20 @@ function clickMenu() {
         </div>
       </div>
       <div
-        class="flex flex-col gap-[1.5em] items-center font-mono font-bold text-lg tracking-wider mb-[3em]"
+        class="flex flex-col gap-[1.5em] items-center lg:items-start font-mono font-bold text-lg tracking-wider mb-[3em]"
       >
         <p class="text-[#837d87]">contact</p>
-        <div class="flex flex-col items-center text-lg text-[#2d2640]">
+        <div class="flex flex-col items-center lg:items-start text-lg text-[#2d2640]">
           <p>sales</p>
           <p>support</p>
           <p>live chat</p>
         </div>
       </div>
       <div
-        class="flex flex-col gap-[1.5em] items-center font-mono font-bold text-lg tracking-wider mb-[3em]"
+        class="flex flex-col gap-[1.5em] items-center lg:items-start font-mono font-bold text-lg tracking-wider mb-[3em]"
       >
         <p class="text-[#837d87]">Others</p>
-        <div class="flex flex-col items-center text-lg text-[#2d2640]">
+        <div class="flex flex-col items-center lg:items-start text-lg text-[#2d2640]">
           <p>careers</p>
           <p>press</p>
           <p>licenses</p>
