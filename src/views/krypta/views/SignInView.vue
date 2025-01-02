c1915be428b78b385772700a0fe3033edac756fd
Damndaniel241 <lombardia241@gmail.com>
2025-01-02 11:44:50 -0800

    created puzzler and styled

--- src/views/krypta/views/SignInView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { SignIn } from 'vue-clerk'
-</script>
-
-<template>
-    <div class="flex justify-center items-center ">
-  <SignIn />
-  </div>
-</template>
--- src/views/krypta/views/SignUpView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { SignUp } from 'vue-clerk'
-</script>
-
-<template>
-     <div class="flex justify-center items-center ">
-  <SignUp  />
-  </div>
-</template>
