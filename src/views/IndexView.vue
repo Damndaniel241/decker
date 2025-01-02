b54672301b4a15dd62ebcc25ec2ed6c768a52209
Damndaniel241 <lombardia241@gmail.com>
2025-01-02 12:20:13 -0800

    removed clerk 5

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -9,7 +9,7 @@
 
   <div class="flex flex-col justify-center gap-1 border-red-500 rounded-md w-[80%] border items-center p-2">
     <RouterLink :to="{name:'InsureView'}" class=" mx-2 underline text-[#be4444]">insure-landing-page</RouterLink>
-    <RouterLink :to="{name:'KryptaIndex'}" class=" mx-2 underline text-[#be4444]">krypta</RouterLink>
+    <!-- <RouterLink :to="{name:'KryptaIndex'}" class=" mx-2 underline text-[#be4444]">krypta</RouterLink> -->
     <RouterLink :to="{name:'PasterIndex'}" class=" mx-2 underline text-[#be4444]">paster</RouterLink>
     <RouterLink :to="{name:'PuzzlerIndex'}" class=" mx-2 underline text-[#be4444]">puzzler</RouterLink>
     <!-- <RouterLink :to="{name:'InsureView'}" class="underline text-[#be4444]">insure-landing-page</RouterLink> -->
