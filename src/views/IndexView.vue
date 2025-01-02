8bfab0e2b65cc61a03dc8cae3c88e3c25dbb57a5
Damndaniel241 <lombardia241@gmail.com>
2025-01-02 10:19:49 -0800

    created puzzler and styled

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -2,12 +2,12 @@
 
 <template>
 
-    <div id="wrapper">
-  <h1 class="text-[#be4444] w-[50%] text-center">
+    <div id="" class="flex justify-center items-center flex-col">
+  <h1 class="text-[#be4444] text-center ">
     hey there this is simply a link to all the other pages
   </h1>
 
-  <div class="flex flex-col gap-1 border-red-500 rounded-md border w-[40%] items-center p-2">
+  <div class="flex flex-col justify-center gap-1 border-red-500 rounded-md w-[80%] border items-center p-2">
     <RouterLink :to="{name:'InsureView'}" class=" mx-2 underline text-[#be4444]">insure-landing-page</RouterLink>
     <RouterLink :to="{name:'KryptaIndex'}" class=" mx-2 underline text-[#be4444]">krypta</RouterLink>
     <RouterLink :to="{name:'PasterIndex'}" class=" mx-2 underline text-[#be4444]">paster</RouterLink>
